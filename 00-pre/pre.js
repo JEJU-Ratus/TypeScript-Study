3763fb691ad16066c7e5b9489ec887b2eade6eec
jema <smh1141@naver.com>
2026-07-27 09:01:58 +0900

    00. pre 강의 추가

---
+++ 00-pre/pre.js
@@ -0,0 +1,3 @@
+"use strict";
+const a = 10;
+a = "hello"; // type 오류
---
+++ 00-pre/pre.ts
@@ -0,0 +1,9 @@
+// const a = 10;
+
+// a = "hello"; // type 오류
+
+function greeting(name) {
+  return name + "어서오세요!";
+}
+
+greeting("tiger");
