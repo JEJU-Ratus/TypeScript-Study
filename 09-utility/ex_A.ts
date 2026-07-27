bf293ceb2671733dd61f9d7b175de5456ae4dfff
jema <smh1141@naver.com>
2026-07-27 13:01:19 +0900

    09 실습 A 및 B

--- 09-utility/ex_A.ts
+++ 09-utility/ex_A.ts
@@ -1,14 +1,16 @@
+import { User } from "./utility";
+
 // 실습 A
 type UserDraftA = Partial<User>;
-const a1: UserDraftA = { name: '홍길동' }; // OK
-const a2: UserDraftA = {};                 // OK
+const a1: UserDraftA = { name: "홍길동" }; // OK
+const a2: UserDraftA = {}; // OK
 
 type UserStrictA = Required<User>;
 /* 아래 객체를 UserStrictA에 맞게 완성해 보자 */
-const a3 /* : UserStrictA */ = {
+const a3: UserStrictA = {
   id: 1,
-  name: 'Kim',
-  role: 'member',
+  name: "Kim",
+  role: "member",
   //   <- 추가 작성 email 속성의 값으로 'kim@example.com'
+  email: "kim@example.com",
 };
-
